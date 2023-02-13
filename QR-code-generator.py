a4368f203c2cf3613193d2cd50bd1f6a7cddd0e3
Gl Tats <74324703+gltats@users.noreply.github.com>
2023-02-13 20:51:19 +0100

    Update QR-code-generator.py

--- QR-code-generator.py
+++ QR-code-generator.py
@@ -9,6 +9,6 @@ def create_qr_with_title(title, url):
 
     qr_img.save("qr_code.png")
 
-url = "https://drive.google.com/file/d/1aJCln75OyTys8CCsslRl7L5JPVIoMKhO/view?usp=share_link"
+url = "https://drive.google.com/file/d/xxxxxxxxx/view?usp=share_link"
 title = "Party Invitation:"
 create_qr_with_title(title, url)
